d411292aeae7b9daa835e967b05fb51bad2a60e5
dyakant <dyakant@gmail.com>
2018-05-17 22:28:02 +0300

    17/05/2018

--- discussion/discussion.js
+++ discussion/discussion.js
@@ -13,9 +13,20 @@ function addComment() {
     if (authorEl.value == "") {
         alert("Author must be filled!")
     }
-    const discussion = document.getElementsByClassName('disc-center')
+    const discussion = document.getElementById('discussion');
     console.log(commentEl.value);
     console.log(authorEl.value);
 
+    let div = document.createElement('div');
+    div.className = 'disc-item';
+    div.innerHTML = `<div class="disc-names">
+    <div>БУ</div><div>17.05.2018<br>14:26:54</div>
+</div>
+<div class="disc-content">
+<div class="disc-content-item">${authorEl.value}<p>${commentEl.value}</p></div>
+</div>`;
+
+    console.log(div.innerHTML);
+    discussion.insertBefore(div, discussion.firstChild);
 }
 
